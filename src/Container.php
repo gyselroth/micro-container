3ba97d6219b09d5aab0691ad7d2bff81b80a43a2
Raffael Sahli <sahli@gyselroth.com>
2018-01-08 13:59:24 +0100

    fixes container

--- src/Container.php
+++ src/Container.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 /**
- * Micro
+ * Balloon
  *
  * @author      Raffael Sahli <sahli@gyselroth.net>
- * @copyright   Copryright (c) 2015-2017 gyselroth GmbH (https://gyselroth.com)
- * @license     MIT https://opensource.org/licenses/MIT
+ * @copyright   Copryright (c) 2012-2017 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
  */
 
 namespace Micro\Container;
@@ -293,7 +293,7 @@ class Container implements ContainerInterface
             }
 
             foreach ($adapters as $adapter => $service) {
-                if (isset($service['enabled']) && '0' === $service['enabled']) {
+                if (isset($service['enabled']) && false == $service['enabled']) {
                     continue;
                 }
 
